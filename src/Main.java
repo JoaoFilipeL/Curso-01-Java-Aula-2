9e0d771c4c75a218106fd0b6de6f415e635018ae
João Filipe <joaofcliotto@gmail.com>
2024-03-14 17:37:30 -0300

    aula 2

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double media = 7.0;
+        System.out.println(media);
+        int classificacao = (int) (media / 2.0);
+        System.out.println(classificacao);
+    }
+}
